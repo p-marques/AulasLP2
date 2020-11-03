80a9c4a1c9cfc7b4b7556ec758218cfc2ec6c9d2
Pedro Dias Marques <pmark.pt@gmail.com>
2020-11-03 12:19:48 +0000

    Add Coroutine exercise

---
+++ Unity_3Nov2020/Assets/Scripts/CRExample.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRExample : MonoBehaviour
+{
+    private Coroutine sayHelloCR;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        sayHelloCR = StartCoroutine(SayHelloCR(4.0f));
+        StartCoroutine(SumUpCR(1.0f));
+    }
+
+    private IEnumerator SayHelloCR(float interval)
+    {
+        WaitForSeconds wait = new WaitForSeconds(interval);
+
+        while (true)
+        {
+            Debug.Log("Hello");
+
+            yield return wait;
+        }
+    }
+
+    private IEnumerator SumUpCR(float interval)
+    {
+        int value = 0;
+        WaitForSeconds wait = new WaitForSeconds(interval);
+
+        while (true)
+        {
+            value += 1;
+
+            if (value >= 30)
+            {
+                StopCoroutine(sayHelloCR);
+                Debug.Log("Stopped saying hello.");
+                yield break; // If we don't want this Coroutine to run forever
+            }
+
+            yield return wait;
+        }
+    }
+}
